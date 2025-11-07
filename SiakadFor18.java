abe74427eb926e7fd9f3c98aab34d59870af2e60
alfatihrafi0-sketch <alfatihrafi0@gmail.com>
2025-11-07 14:16:35 +0700

    SiakadFor

---
+++ SiakadFor18.java
@@ -0,0 +1,32 @@
+import java.util.Scanner; 
+    public class SiakadFor18 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidaklulus =0;
+
+        for(int i = 1; i <= 10; i++) {
+
+                System.out.print("Masukan nilai mahasiswa ke-" + i + ":");
+                nilai = sc.nextDouble();
+                if (nilai > tertinggi){
+                    tertinggi = nilai;
+                }
+                if (nilai < terendah){
+                    terendah = nilai;
+                }
+                if (nilai <=60) {
+                    tidaklulus++;
+                } else{
+                    lulus++;
+                }
+            }
+            System.out.println("Nilai tertinggi: " + tertinggi);
+            System.out.println("Nilai terendah: " + terendah);
+            System.out.println("Jumlah siswa lulus :"+lulus);
+            System.out.println("Jumlah siswa tidak lulus : "+tidaklulus);
+        }
+
+    }
+
